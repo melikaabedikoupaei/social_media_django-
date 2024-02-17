264c680de87fbede4c1b44f23015e34be8b575fd
mabediko <melika.abedikoupaei@ugent.be>
2024-02-17 13:26:54 +0100

    customize admin page for profile model

--- social_media/core/admin.py
+++ social_media/core/admin.py
@@ -2,7 +2,12 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(Profile)
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display=('user','id_user','location')
+    list_filter=('location',)
+    ordering=["id_user"]
+    search_fields=('user','id_user','location')
 admin.site.register(Post)
 admin.site.register(LikePost)
 admin.site.register(FollowersCount)
