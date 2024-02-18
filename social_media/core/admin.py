0e86017f88eb87c404ac533902b0f9f761921b0c
mabediko <melika.abedikoupaei@ugent.be>
2024-02-18 11:11:04 +0100

    Revert "class Meta in model for post"
    This reverts commit 05731e9dbedfe7b798a3c53ff4313f9d59a88a0a.

--- social_media/core/admin.py
+++ social_media/core/admin.py
@@ -8,9 +8,6 @@ class ProfileAdmin(admin.ModelAdmin):
     list_filter=('location',)
     ordering=["id_user"]
     search_fields=('user','id_user','location')
-@admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
-    list_display=('user','created_at','no_of_likes')
-    search_fields=('user','created_at','no_of_likes')
+admin.site.register(Post)
 admin.site.register(LikePost)
 admin.site.register(FollowersCount)
