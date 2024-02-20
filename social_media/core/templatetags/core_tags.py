2eec4c47dc4869f054e1ba9b79eb969aa498883d
mabediko <melika.abedikoupaei@ugent.be>
2024-02-20 15:22:20 +0100

    number of total like for each user

--- social_media/core/templatetags/core_tags.py
+++ social_media/core/templatetags/core_tags.py
@@ -1,7 +1,16 @@
 from django import template
-from  core.models import *
+from core.models import Post
+
 register = template.Library()
 
 @register.simple_tag(name='totallike')
-def function():
-    return 10
+def function(request):
+    username = request.user.username
+    total_likes_count = 0
+    posts = Post.objects.filter(user=username)
+    for post in posts:
+        total_likes_count += post.no_of_likes
+    return total_likes_count
+
+
+
